7800f1ae597a3709a3815db2a568c458da5f228a
30087241 <30087241@nescol.ac.uk>
2023-08-30 10:58:34 +0100

    hi

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+* This is my first Java program designed to display the string Hello World
+* @author Dominic Cameron
+ * @version 1
+        */
+public class HelloWorld {
+    // Write out 'Hello World' to the program
+    public static void main (String[] args) {
+    System.out.println("Hello world!");
+
+    }
+}
