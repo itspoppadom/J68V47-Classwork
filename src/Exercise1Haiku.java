22a65266d93b12b1a7911b9254c029209833b6ae
30087241 <30087241@nescol.ac.uk>
2023-08-30 11:07:39 +0100

    hi

---
+++ src/Exercise1Haiku.java
@@ -0,0 +1,7 @@
+public class Exercise1Haiku {
+    public static void main (String[] args) {
+        System.out.println("The world is on fire");
+        System.out.println("Yet we still fight over things");
+        System.out.println("That don't matter at all.");
+    }
+}
