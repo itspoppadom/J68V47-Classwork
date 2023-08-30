4adc9a66e4caf2d27a7540f0db8b01c67e2f2173
30087241 <30087241@nescol.ac.uk>
2023-08-30 11:10:16 +0100

    hi

--- src/Exercise1Haiku.java
+++ src/Exercise1Haiku.java
@@ -1,3 +1,7 @@
+/** Exercise 1: Display a Haiku
+ * @author Dominic Cameron
+ * @version 1
+ */
 public class Exercise1Haiku {
     public static void main (String[] args) {
         System.out.println("The world is on fire");
