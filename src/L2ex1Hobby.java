19ecc0c4cbedae533cb8901482fbd48f91279540
30087241 <30087241@nescol.ac.uk>
2023-09-06 11:39:10 +0100

    Lessom 2 Java

---
+++ src/L2ex1Hobby.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class L2ex1Hobby {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("I'd like to get to know you");
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+
+        System.out.println("And, what's your favourite hobby?");
+        String favHobby = input.nextLine();
+
+        System.out.println("");
+    }
+}
--- src/NameInput.java
+++ src/NameInput.java
@@ -3,18 +3,20 @@ public class NameInput {
 
     public static void main(String[] args) {
 
-
+        System.out.println("MPG Calculator");
+        System.out.println("Use this program to calculate how many miles you managed to drive on your tank of petrol!");
         Scanner input = new Scanner(System.in);
-        System.out.print("What is your name? ");
-        String name = input.nextLine();
-        System.out.println(name);
+        System.out.print("What mileage was your car when you filled up your car? ");
+        int MileageA = input.nextInt();
 
-        System.out.print("What is your first name? ");
-        String firstName = input.nextLine();
+        System.out.print("How many gallons of fuel did you fill up?");
+        float FuelCap = input.nextFloat();
 
-        System.out.print("What is your last name? ");
-        String lastName = input.nextLine();
+        System.out.print("What is your mileage now? ");
+        int MileageB = input.nextInt();
 
-        System.out.println("Hello " + firstName +" " +lastName);
+        int MilesCovered = (MileageB - MileageA) ;
+        float MPG = MilesCovered/FuelCap ;
+        System.out.format("Your car has the efficiency of " + MPG + "Miles Per Gallon");
     }
 }
