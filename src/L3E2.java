3b720935e9fa6fa1e378705a378d144ecbc66e46
30087241 <30087241@nescol.ac.uk>
2023-09-13 11:12:49 +0100

    Lessom 3

--- src/L3E2.java
+++ src/L3E2.java
@@ -3,10 +3,18 @@ public class L3E2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("What is the capital of Spain?");
-        String Answer1 = input.next();
+        String Answer1 = input.nextLine();
+        String CheckAns1 = Answer1.toLowerCase();
+        System.out.println(CheckAns1.contains("madrid"));
+        Scanner input2 = new Scanner(System.in);
         System.out.println("What is the capital of UK?");
-        String Answer2 = input.next();
+        String Answer2 = input2.nextLine();
+        String CheckAns2 = Answer2.toLowerCase();
+        System.out.println(CheckAns2.contains("london"));
+        Scanner input3 = new Scanner(System.in);
         System.out.println("What is the capital of Italy");
-        String Answer3 = input.next();
+        String Answer3 = input3.nextLine();
+        String CheckAns3 = Answer3.toLowerCase();
+        System.out.println(CheckAns3.contains("rome"));
     }
 }
