3537fbd72644cf8234734fb7c5dcc3d067ef9e84
30087241 <30087241@nescol.ac.uk>
2023-10-04 10:08:38 +0100

    Lesson 4 Code and Fixes

---
+++ src/L6Ex1.java
@@ -0,0 +1,9 @@
+public class L6Ex1 {
+    public static void main(String[] args) {
+        int count = 10 ;
+        while (count <=24) {
+            System.out.println(count);
+            count+=2;
+        }
+    }
+}
